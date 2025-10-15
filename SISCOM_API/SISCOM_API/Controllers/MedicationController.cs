81780f00d11eafdbd2887da295dd8dd8b8a630ba
eli <robersonhernandez1@gmail.com>
2025-10-15 16:52:42 -0600

    Actualización: controladores listos y Swagger funcionando correctamente, se creo base de datos

---
+++ SISCOM_API/SISCOM_API/Controllers/MedicationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SISCOM_API.Data;
+using SISCOM_API.Models;
+
+namespace SISCOM_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicationController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MedicationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddMedication([FromBody] Medication medication)
+        {
+            if (medication == null)
+                return BadRequest("El medicamento no puede estar vacío.");
+
+            _context.Medications.Add(medication);
+            _context.SaveChanges();
+            return Ok(medication);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _context.Medications.ToList();
+            return Ok(list);
+        }
+    }
+}
---
+++ SISCOM_API/SISCOM_API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SISCOM_API.Models;
+
+namespace SISCOM_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Medication> Medications { get; set; }
+    }
+}
