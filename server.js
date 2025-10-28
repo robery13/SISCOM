f17a47d06e1580f8ed3de33f80bc6fd6f93db645
JONATHAN DANIEL MENJIVAR CHIU <jdmenjivar@unah.hn>
2025-10-27 20:08:20 -0600

    Agregado menú prototipo y actualización de Proyect o_SISCOM, script_main y server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // Configura tu conexión MySQL (datos de conexion a  la base de datos)
 //agregar los datos antes de ejecutar y quitarlos antes de commit
 
+
 //mensaje de error o exito de conexion
 db.connect(err => {
   if (err) {
@@ -40,4 +41,43 @@ app.post('/guardarMedicamento', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
+});
+
+
+
+
+//script login
+
+// Verificar correo y contraseña al iniciar sesión
+app.post('/login', (req, res) => {
+  
+const { email, password } = req.body;
+console.log("Datos recibidos:", req.body);
+
+
+
+  // Consulta SQL para buscar el usuario por correo
+  const sql = 'SELECT * FROM usuarios WHERE email = ?';
+
+  db.query(sql, [email], (err, results) => {
+    if (err) {
+      console.error('Error al consultar la base de datos:', err);
+      return res.status(500).json({ mensaje: 'Error interno del servidor' });
+    }
+
+    // Si no se encontró ningún usuario con ese correo
+    if (results.length === 0) {
+      return res.status(401).json({ mensaje: 'Correo o contraseña incorrectos' });
+    }
+
+    const usuario = results[0];
+
+    // Comparar la contraseña ingresada con la almacenada
+    if (password !== usuario.password) {
+      return res.status(401).json({ mensaje: 'Correo o contraseña incorrectos' });
+    }
+
+    // Si llega aquí, el login es correcto
+    res.json({ mensaje: 'Inicio de sesión exitoso', usuario });
+  });
 });
